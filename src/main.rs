f2dfeb8a13383155fb5ac8b8ddc0d11c297ee813
alexander-drabik <xelada91@o2.pl>
2022-07-06 13:04:10 +0200

    Get code from file given in arguments

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
+use std::{env, fs};
+
 mod lexer;
 
 fn main() {
-    let tokens = lexer::tokenize("print(1)");
-    for token in tokens {
-        println!("{}", token.value)
+    // Get code from file
+    let args: Vec<_> = env::args().collect();
+    if !args.len() > 1 {
+        println!("No file given. Use: yapko [filename]");
+        return;
     }
+    let code = get_file_content(&args[1]);
+}
+
+fn get_file_content(filename: &String) -> String {
+    return fs::read_to_string(filename)
+        .expect("Cannot open file!");
 }
