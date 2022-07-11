ad0354b0f49ecaa99b83420b143902ab925f0910
alexander-drabik <xelada91@o2.pl>
2022-07-11 09:58:41 +0200

    Make primitives and objects stored in one hashmap

--- src/VM/yapko.rs
+++ src/VM/yapko.rs
@@ -1,25 +1,39 @@
 use std::collections::HashMap;
 
+macro_rules! hashmap {
+    ($( $key: expr => $val: expr ),*) => {{
+         let mut map = ::std::collections::HashMap::new();
+         $( map.insert($key, $val); )*
+         map
+    }}
+}
+
+#[derive(Clone)]
+pub enum Variable {
+    Primitive(Primitive),
+    YapkoObject(YapkoObject),
+
+}
+
 #[derive(Clone)]
 pub enum Primitive {
     Int(i32),
-    String(String)
+    String(String),
+    Null
 }
 
 #[derive(Clone)]
 pub struct YapkoObject {
     pub name: String,
     pub yapko_type: String,
-    pub variables: HashMap<String, YapkoObject>,
-    pub primitive: Primitive
+    pub variables: HashMap<String, Variable>,
 }
 
 pub fn generate_int(name: String, value: i32) -> YapkoObject {
     YapkoObject {
         name,
         yapko_type: "Int".parse().unwrap(),
-        variables: Default::default(),
-        primitive: Primitive::Int(value)
+        variables: hashmap![String::from("value") => Variable::Primitive(Primitive::Int(0))]
     }
 }
 
@@ -27,7 +41,6 @@ pub fn generate_null(name: String) -> YapkoObject {
     YapkoObject {
         name,
         yapko_type: "Null".parse().unwrap(),
-        variables: Default::default(),
-        primitive: Primitive::Int(0)
+        variables: hashmap!(String::from("value") => Variable::Primitive(Primitive::Null))
     }
 }
